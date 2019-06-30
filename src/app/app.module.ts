f8f9380c7d4f575d567c84f78a4d472c29f92973
aziziyazit <aziziyazit@gmail.com>
2019-07-01 05:55:56 +0800

    fix module atomic component import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
-import jss from 'jss';
-import preset from 'jss-preset-default';
+import jss from "jss";
+import preset from "jss-preset-default";
 
-import { AtomicComponentModule } from "projects/atomic-component/src/public-api";
+import { AtomicComponentModule } from "./../../projects/atomic-component/src/public-api";
 
 jss.setup(preset());
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AtomicComponentModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, AtomicComponentModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
