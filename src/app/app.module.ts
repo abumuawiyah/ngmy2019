718e69ebc7f7917f5b21d0e716e8ebb79362a574
aziziyazit <aziziyazit@gmail.com>
2019-07-01 06:35:25 +0800

    remove jss from app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,7 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
-import jss from "jss";
-import preset from "jss-preset-default";
-
 import { AtomicComponentModule } from "./../../projects/atomic-component/src/public-api";
-
-jss.setup(preset());
 
 @NgModule({
   declarations: [AppComponent],
